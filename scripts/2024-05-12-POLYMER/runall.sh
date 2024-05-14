6ae5b3eae796ff4556ce10f9d59b53b0c59a2d45
Simon Gravelle <simon.gravelle@live.fr>
2024-05-14 23:48:49 +0200

    added files

---
+++ scripts/2024-05-12-POLYMER/runall.sh
@@ -0,0 +1,3 @@
+mpirun -np 8 /home/simon/Softwares/lammps-2Aug2023/src/lmp_mpi -in create.lammps
+mpirun -np 8 /home/simon/Softwares/lammps-2Aug2023/src/lmp_mpi -in equilibrate.lammps
+mpirun -np 8 /home/simon/Softwares/lammps-2Aug2023/src/lmp_mpi -in prod.lammps
