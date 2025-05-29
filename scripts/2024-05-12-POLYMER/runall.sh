0841fb0e2ca87560238cead6428ab6809d4996d0
Simon Gravelle <simon.gravelle@live.fr>
2025-05-29 12:54:33 +0200

    geenrated polymer in water images

--- scripts/2024-05-12-POLYMER/runall.sh
+++ scripts/2024-05-12-POLYMER/runall.sh
@@ -1,3 +1,3 @@
-mpirun -np 8 /home/simon/Softwares/lammps-2Aug2023/src/lmp_mpi -in create.lammps
-mpirun -np 8 /home/simon/Softwares/lammps-2Aug2023/src/lmp_mpi -in equilibrate.lammps
-mpirun -np 8 /home/simon/Softwares/lammps-2Aug2023/src/lmp_mpi -in prod.lammps
+mpirun -np 8 /home/simon/Softwares/lammps-19Nov2024/src/lmp_mpi -in create.lammps
+mpirun -np 8 /home/simon/Softwares/lammps-19Nov2024/src/lmp_mpi -in equilibrate.lammps
+mpirun -np 8 /home/simon/Softwares/lammps-19Nov2024/src/lmp_mpi -in prod.lammps
